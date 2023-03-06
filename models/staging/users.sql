129c0832aaa6f29dfb8c9db554d9b3addfc7fd62
Alexandre <alex.boutou@gmail.com>
2023-03-06 14:13:54 +0100

    add users model

---
+++ models/staging/users.sql
@@ -0,0 +1,14 @@
+SELECT  
+    user_id
+    , birth_year
+    , country
+    , city
+    , created_date
+    , user_settings_crypto_unlocked
+    , plan
+    , attributes_notifications_marketing_push
+    , attributes_notifications_marketing_email
+    , num_contacts
+    , num_referrals
+    , num_successful_referrals
+FROM raw_data.users
