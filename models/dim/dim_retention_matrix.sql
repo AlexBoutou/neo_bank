e670c7191e84a9f3f60fcaccd64a4547cb04eae9
Alexandre <alex.boutou@gmail.com>
2023-03-07 17:05:35 +0100

    add absolute number retention matrix

--- models/dim/dim_retention_matrix.sql
+++ models/dim/dim_retention_matrix.sql
@@ -5,6 +5,13 @@ WITH initial_month AS (
     FROM {{ ref('dim_user_event_with_sold') }}
     WHERE event_type = "create_account"
 ),
+cohort_size AS (
+    SELECT
+        month_year,
+        count(user_id) as cohort_size
+    FROM initial_month
+    GROUP BY 1
+),
 active_month_users AS (
     SELECT
         user_id,
@@ -22,62 +29,48 @@ nb_month_users AS (
     FROM active_month_users act
     LEFT JOIN initial_month ini using(user_id)
     order by act.user_id,act.month_year
-),
-absolute_number as (
-    SELECT 
-    ini.month_year,
-    SUM(CASE 
-        WHEN us.mn = 1 THEN 1
-        ELSE 0 END) as one,
-    SUM(CASE 
-        WHEN us.mn = 2 THEN 1
-        ELSE 0 END) as two,
-    SUM(CASE 
-        WHEN us.mn = 3 THEN 1
-        ELSE 0 END) as three,
-    SUM(CASE 
-        WHEN us.mn = 4 THEN 1
-        ELSE 0 END) as four,
-    SUM(CASE 
-        WHEN us.mn = 5 THEN 1
-        ELSE 0 END) as five,
-    SUM(CASE 
-        WHEN us.mn = 6 THEN 1
-        ELSE 0 END) as six,
-    SUM(CASE 
-        WHEN us.mn = 7 THEN 1
-        ELSE 0 END) as seven,
-    SUM(CASE 
-        WHEN us.mn = 8 THEN 1
-        ELSE 0 END) as eight,
-    SUM(CASE 
-        WHEN us.mn = 9 THEN 1
-        ELSE 0 END) as nine,
-    SUM(CASE 
-        WHEN us.mn = 10 THEN 1
-        ELSE 0 END) as ten,
-    SUM(CASE 
-        WHEN us.mn = 11 THEN 1
-        ELSE 0 END) as eleven,
-    SUM(CASE 
-        WHEN us.mn = 12 THEN 1
-        ELSE 0 END) as twelve
-    FROM initial_month ini 
-    LEFT JOIN nb_month_users us using(user_id)
-    GROUP BY ini.month_year
-    order by month_year)
-SELECT
-    month_year,
-    1 as one,
-    ROUND(two/one,2) as two,
-    ROUND(three/one,2) as three,
-    ROUND(four/one,2) as four,
-    ROUND(five/one,2) as five,
-    ROUND(six/one,2) as six,
-    ROUND(seven/one,2) as seven,
-    ROUND(eight/one,2) as eight,
-    ROUND(nine/one,2) as nine,
-    ROUND(ten/one,2) as ten,
-    ROUND(eleven/one,2) as eleven,
-    ROUND(twelve/one,2) as twelve
-FROM absolute_number
+)
+SELECT 
+ini.month_year,
+cs.cohort_size,
+SUM(CASE 
+    WHEN us.mn = 1 THEN 1
+    ELSE 0 END) as one,
+SUM(CASE 
+    WHEN us.mn = 2 THEN 1
+    ELSE 0 END) as two,
+SUM(CASE 
+    WHEN us.mn = 3 THEN 1
+    ELSE 0 END) as three,
+SUM(CASE 
+    WHEN us.mn = 4 THEN 1
+    ELSE 0 END) as four,
+SUM(CASE 
+    WHEN us.mn = 5 THEN 1
+    ELSE 0 END) as five,
+SUM(CASE 
+    WHEN us.mn = 6 THEN 1
+    ELSE 0 END) as six,
+SUM(CASE 
+    WHEN us.mn = 7 THEN 1
+    ELSE 0 END) as seven,
+SUM(CASE 
+    WHEN us.mn = 8 THEN 1
+    ELSE 0 END) as eight,
+SUM(CASE 
+    WHEN us.mn = 9 THEN 1
+    ELSE 0 END) as nine,
+SUM(CASE 
+    WHEN us.mn = 10 THEN 1
+    ELSE 0 END) as ten,
+SUM(CASE 
+    WHEN us.mn = 11 THEN 1
+    ELSE 0 END) as eleven,
+SUM(CASE 
+    WHEN us.mn = 12 THEN 1
+    ELSE 0 END) as twelve
+FROM initial_month ini 
+LEFT JOIN nb_month_users us on ini.user_id = us.user_id
+LEFT JOIN cohort_size cs on ini.month_year = cs.month_year
+GROUP BY 1,2
+order by month_year
