225f6054c55a24a33a3093da09aa1cc74f378b07
Alexandre <alex.boutou@gmail.com>
2023-03-07 15:36:47 +0100

    add user_monthly model

---
+++ models/dim/dim_user_event_monthly.sql
@@ -0,0 +1,10 @@
+SELECT
+    ev.user_id,
+    DATE(EXTRACT(YEAR from ev.timestamp),EXTRACT(MONTH from ev.timestamp),1) as month,
+    ev.event_type,
+    ev.event_detail,
+    count(*) as nb_event,
+    ROUND(sum(ev.amount),2) as amount_sum
+FROM {{ ref('dim_user_event_with_sold') }} ev
+group by 1,2,3,4
+order by 1,2,3
