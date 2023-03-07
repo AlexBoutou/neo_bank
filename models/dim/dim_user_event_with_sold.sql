d296f988240ecd8afa218cbc913bd5ebb58e2ce4
Alexandre <alex.boutou@gmail.com>
2023-03-07 16:55:39 +0100

    add event status to int_users_event

--- models/dim/dim_user_event_with_sold.sql
+++ models/dim/dim_user_event_with_sold.sql
@@ -4,6 +4,7 @@ SELECT
     , event_type
     , event_detail
     , amount
+    , event_status
     , ROUND(SUM(amount) OVER(PARTITION BY user_id ORDER BY timestamp RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW),2) as sold
 FROM {{ ref('int_users_event') }}
 ORDER BY user_id, timestamp
