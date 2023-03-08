e34dfd5851c21e133c55a1210f1249546757545b
Alexandre <alex.boutou@gmail.com>
2023-03-08 11:11:58 +0100

    rename column in int_users_last_transaction

--- models/intermediate/events/int_users_last_transaction.sql
+++ models/intermediate/events/int_users_last_transaction.sql
@@ -3,12 +3,12 @@ SELECT
 FROM (
     SELECT DISTINCT 
         user_id
-        , event_type
-        , event_detail
-        , event_status
-        , timestamp
-        , amount
-        , sold
+        , event_type as last_transaction_event_type
+        , event_detail as last_transaction_event_detail
+        , event_status as last_transaction_event_status
+        , timestamp as last_transaction_timestamp
+        , amount as last_transaction_amount
+        , sold as last_transaction_sold
         , ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY timestamp DESC) as rn
     FROM {{ ref('int_user_event_with_sold') }}
     WHERE event_type = "transaction"
