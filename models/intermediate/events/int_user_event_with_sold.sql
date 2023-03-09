f339886876dcc74387e303316cea53ca02e92a4d
Alexandre <alex.boutou@gmail.com>
2023-03-09 15:51:23 +0100

    correct sold calculation

--- models/intermediate/events/int_user_event_with_sold.sql
+++ models/intermediate/events/int_user_event_with_sold.sql
@@ -5,6 +5,6 @@ SELECT
     , event_detail
     , amount
     , event_status
-    , ROUND(SUM(amount) OVER(PARTITION BY user_id ORDER BY timestamp RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW),2) as sold
+    , ROUND(SUM(IF(event_status = 'completed',amount,0)) OVER(PARTITION BY user_id ORDER BY timestamp RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW),2) as sold
 FROM {{ ref('int_users_event') }}
 ORDER BY user_id, timestamp
