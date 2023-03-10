1477eae5ec4eda86964d52437f4ecf53ea577f2c
Alexandre <alex.boutou@gmail.com>
2023-03-10 11:18:07 +0100

    change filtering logic in life_summary models

--- models/intermediate/life_summary/int_users_activity_period.sql
+++ models/intermediate/life_summary/int_users_activity_period.sql
@@ -1,17 +1,44 @@
-with user_cross_month AS (
+WITH user_cross_month AS (
     SELECT
         user_id,
         month_year
-    FROM {{ ref('stg_users') }}
-    CROSS JOIN {{ ref('int_event_months') }}
-    order by 1,2)
+    FROM
+        {{ ref('stg_users') }}
+        CROSS JOIN {{ ref('int_event_months') }}
+    ORDER BY
+        1,
+        2
+)
 SELECT
-    um.user_id,
-    um.month_year,
-    CASE 
-        WHEN ut.user_id is not null THEN 1
-        ELSE 0
-    END as is_active_month
-FROM user_cross_month um
-LEFT JOIN {{ ref("int_users_with_transactions_month") }} ut on um.user_id = ut.user_id and um.month_year = ut.month_year
-ORDER BY 1,2
+    user_id,
+    month_year,
+    is_active_month,
+    FORMAT_DATETIME("%Y, %m",created_date) as created_date
+FROM
+    (
+        SELECT
+            um.user_id,
+            um.month_year,
+            CASE
+                WHEN ut.user_id IS NOT NULL THEN 1
+                ELSE 0
+            END AS is_active_month,
+            date_diff(parse_datetime("%Y, %m", um.month_year), EXTRACT(datetime
+        FROM
+            us.created_date), MONTH) AS diff_with_creation_month,
+            us.created_date as created_date
+        FROM
+            user_cross_month um
+            LEFT JOIN {{ ref("int_users_with_transactions_month") }}
+            ut
+            ON um.user_id = ut.user_id
+            AND um.month_year = ut.month_year
+            LEFT JOIN {{ ref('stg_users') }}
+            us
+            ON um.user_id = us.user_id
+        ORDER BY
+            1,
+            2
+    )
+WHERE
+    diff_with_creation_month >= 0
