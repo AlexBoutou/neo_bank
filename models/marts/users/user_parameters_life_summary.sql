59c75b3046d067875ec16e6effee4652e6ee75c3
gilois.jocelyn@gmail.com <gilois.jocelyn@gmail.com>
2023-03-13 13:53:57 +0000

    commit

--- models/marts/users/user_parameters_life_summary.sql
+++ models/marts/users/user_parameters_life_summary.sql
@@ -17,28 +17,27 @@ SELECT
     pa.email,
     pa.push,
 -- int_users_event_pivot + int_lifetime_user 
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_topup,li.lifetime_month),3),0) as avgmonth_amount_topup,
-    IFNULL(round(SAFE_DIVIDE(ev.count_topup,li.lifetime_month),3),0) as avgmonth_count_topup,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_transfer,li.lifetime_month),3),0) as avgmonth_amount_transfer,
-    IFNULL(round(SAFE_DIVIDE(ev.count_transfer,li.lifetime_month),3),0) as avgmonth_count_transfer,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_tax,li.lifetime_month),3),0) as avgmonth_amount_tax,
-    IFNULL(round(SAFE_DIVIDE(ev.count_tax,li.lifetime_month),3),0) as avgmonth_count_tax,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_refunds,li.lifetime_month),3),0) as avgmonth_amount_refunds,
-    IFNULL(round(SAFE_DIVIDE(ev.count_refunds,li.lifetime_month),3),0) as avgmonth_count_refunds,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_fee,li.lifetime_month),3),0) as avgmonth_amount_fee,
-    IFNULL(round(SAFE_DIVIDE(ev.count_fee,li.lifetime_month),3),0) as avgmonth_count_fee,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_exchange,li.lifetime_month),3),0) as avgmonth_amount_exchange,
-    IFNULL(round(SAFE_DIVIDE(ev.count_exchange,li.lifetime_month),3),0) as avgmonth_count_exchange,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_cashback,li.lifetime_month),3),0) as avgmonth_amount_cashback,
-    IFNULL(round(SAFE_DIVIDE(ev.count_cashback,li.lifetime_month),3),0) as avgmonth_count_cashback,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_card_payment,li.lifetime_month),3),0) as avgmonth_amount_card_payment,
-    IFNULL(round(SAFE_DIVIDE(ev.count_card_payment,li.lifetime_month),3),0) as avgmonth_count_card_payment,
-    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_card_atm,li.lifetime_month),3),0) as avgmonth_amount_atm,
-    IFNULL(round(SAFE_DIVIDE(ev.count_atm,li.lifetime_month),3),0) as avgmonth_count_atm, 
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_topup,li.lifetime_month),2),0) as avgmonth_amount_topup,
+    IFNULL(round(SAFE_DIVIDE(ev.count_topup,li.lifetime_month),2),0) as avgmonth_count_topup,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_transfer,li.lifetime_month),2),0) as avgmonth_amount_transfer,
+    IFNULL(round(SAFE_DIVIDE(ev.count_transfer,li.lifetime_month),2),0) as avgmonth_count_transfer,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_tax,li.lifetime_month),2),0) as avgmonth_amount_tax,
+    IFNULL(round(SAFE_DIVIDE(ev.count_tax,li.lifetime_month),2),0) as avgmonth_count_tax,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_refunds,li.lifetime_month),2),0) as avgmonth_amount_refunds,
+    IFNULL(round(SAFE_DIVIDE(ev.count_refunds,li.lifetime_month),2),0) as avgmonth_count_refunds,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_fee,li.lifetime_month),2),0) as avgmonth_amount_fee,
+    IFNULL(round(SAFE_DIVIDE(ev.count_fee,li.lifetime_month),2),0) as avgmonth_count_fee,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_exchange,li.lifetime_month),2),0) as avgmonth_amount_exchange,
+    IFNULL(round(SAFE_DIVIDE(ev.count_exchange,li.lifetime_month),2),0) as avgmonth_count_exchange,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_cashback,li.lifetime_month),2),0) as avgmonth_amount_cashback,
+    IFNULL(round(SAFE_DIVIDE(ev.count_cashback,li.lifetime_month),2),0) as avgmonth_count_cashback,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_card_payment,li.lifetime_month),2),0) as avgmonth_amount_card_payment,
+    IFNULL(round(SAFE_DIVIDE(ev.count_card_payment,li.lifetime_month),2),0) as avgmonth_count_card_payment,
+    IFNULL(round(SAFE_DIVIDE(ev.sum_amount_card_atm,li.lifetime_month),2),0) as avgmonth_amount_atm,
+    IFNULL(round(SAFE_DIVIDE(ev.count_atm,li.lifetime_month),2),0) as avgmonth_count_atm, 
 -- users_parameters_model    
     pa.final_sold,
 -- int_lifetime_user
-    li.user_id,
     li.lifetime_month,
     li.nb_active_period,
     li.nb_inactive_period,
@@ -47,6 +46,6 @@ SELECT
     li.inactive_ratio,
     li.active_ratio,
     li.is_active
-FROM {{ ref ("users_parameters_model")}} pa 
-left join {{ ref("int_users_event_pivot") }} ev on pa.user_id=ev.user_id
-LEFT JOIN {{ ref ("int_lifetime_user")}} li on pa.user_id=li.user_id
+FROM {{ ref ("users_parameters_model")}} pa
+LEFT JOIN {{ ref("int_users_event_pivot") }} ev on pa.user_id=ev.user_id
+LEFT JOIN {{ ref ("users_life_summary")}} li on pa.user_id=li.user_id
