93aff2d9383c514e0b5a703e555668cada978669
Alexandre <alex.boutou@gmail.com>
2023-03-13 15:44:12 +0100

    update user eventm onthly

--- models/marts/events/user_event_monthly.sql
+++ models/marts/events/user_event_monthly.sql
@@ -1,10 +1,12 @@
 SELECT
     ev.user_id,
     DATE(EXTRACT(YEAR from ev.timestamp),EXTRACT(MONTH from ev.timestamp),1) as month,
+    1+DATE_DIFF(DATE(ev.timestamp), DATE(us.created_date), MONTH) as month_nb,
     ev.event_type,
     ev.event_detail,
     count(*) as nb_event,
     ROUND(sum(ev.amount),2) as amount_sum
 FROM {{ ref('int_user_event_with_sold') }} ev
-group by 1,2,3,4
+LEFT JOIN {{ ref('stg_users') }} us using (user_id)
+group by 1,2,3,4,5
 order by 1,2,3
