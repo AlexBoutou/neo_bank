87269235f5a7fe47be15685a5174a0bca92cd125
Alexandre <alex.boutou@gmail.com>
2023-03-13 11:48:10 +0100

    correct life summary

--- models/intermediate/life_summary/int_users_activity_period.sql
+++ models/intermediate/life_summary/int_users_activity_period.sql
@@ -23,22 +23,12 @@ FROM
                 WHEN ut.user_id IS NOT NULL THEN 1
                 ELSE 0
             END AS is_active_month,
-            date_diff(parse_datetime("%Y, %m", um.month_year), EXTRACT(datetime
-        FROM
-            us.created_date), MONTH) AS diff_with_creation_month,
+            date_diff(parse_datetime("%Y, %m", um.month_year), EXTRACT(datetime FROM us.created_date), MONTH) AS diff_with_creation_month,
             us.created_date as created_date
-        FROM
-            user_cross_month um
-            LEFT JOIN {{ ref("int_users_with_transactions_month") }}
-            ut
-            ON um.user_id = ut.user_id
-            AND um.month_year = ut.month_year
-            LEFT JOIN {{ ref('stg_users') }}
-            us
-            ON um.user_id = us.user_id
-        ORDER BY
-            1,
-            2
+        FROM user_cross_month um 
+        LEFT JOIN {{ ref("int_users_with_transactions_month") }} ut ON um.user_id = ut.user_id AND um.month_year = ut.month_year
+        LEFT JOIN {{ ref('stg_users') }} us ON um.user_id = us.user_id
+        ORDER BY 1,2
     )
 WHERE
     diff_with_creation_month >= 0
--- models/marts/users_life/users_life_summary.sql
+++ models/marts/users_life/users_life_summary.sql
@@ -1,7 +1,7 @@
 WITH preprocess AS (
     SELECT
         user_id,
-        sum(nb_month) as lifetime_month,
+        MAX(1+COALESCE(DATE_DIFF(DATE(lf.last_transaction_timestamp),DATE(lf.account_creation_timestamp), MONTH),0)) as lifetime_month,
         SUM(
             CASE 
                 WHEN is_active_month=1 THEN 1
@@ -9,7 +9,7 @@ WITH preprocess AS (
             END) AS nb_active_period,
         SUM(
             CASE 
-                WHEN is_active_month=1 THEN 0
+                WHEN is_active_month=0 THEN 1
                 ELSE 0
             END) AS nb_inactive_period,
         SUM(
@@ -23,17 +23,19 @@ WITH preprocess AS (
                 ELSE 0
             END) AS nb_active_month,
     FROM {{ ref('int_users_life_split') }}
+    LEFT JOIN {{ ref('int_lifetime_user') }} lf using(user_id)
+    WHERE is_last_period !=1 OR is_active_month=1
     group by 1)
 SELECT 
     li.user_id
-    , DATE_DIFF(DATE(lf.last_transaction_timestamp),DATE(lf.account_creation_timestamp), MONTH) as lifetime_month
+    , li.lifetime_month
     , li.nb_active_period
     , li.nb_inactive_period
     ,li.nb_inactive_month
     ,li.nb_active_month
-    , ROUND(li.nb_inactive_month/li.lifetime_month,2) as inactive_ratio
-    , ROUND(li.nb_active_month/li.lifetime_month,2) as active_ratio
+    , ROUND(SAFE_DIVIDE(li.nb_inactive_month,li.lifetime_month),2) as inactive_ratio
+    , ROUND(SAFE_DIVIDE(li.nb_active_month,li.lifetime_month),2) as active_ratio
     , us.is_active
 FROM preprocess li
 LEFT JOIN {{ ref('int_lifetime_user') }} us using(user_id)
-LEFT JOIN {{ ref('int_lifetime_user') }} lf on li.user_id = lf.user_id
+ORDER BY 1
